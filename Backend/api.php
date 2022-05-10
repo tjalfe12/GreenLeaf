9f3a53c3b92289a034f672a05a75a6b6c670242c
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2022-05-10 20:23:17 +0200

    Update api.php
    Added another api endpoint and some comments.

--- Backend/api.php
+++ Backend/api.php
@@ -5,23 +5,34 @@ header("Access-Control-Allow-Methods: *");
 header("Content-Type: application/json; charset=UTF-8");
 include_once("MySQL.php");
 
-$request_method = $_SERVER['REQUEST_METHOD'];
+// Instantiates a MySQL object with auto-connect enabled (the parameter is set to true).
 $mySQL = new MySQL(true);
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getallposts'])) {
-    $sql = "SELECT * FROM `GetPosts`;";
-    echo $mySQL->Query($sql, true);
-} else if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getusers'])) {
+//API endpoints for the GET method. Used to retrieve data.
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getusers'])) {
     $sql = "SELECT * FROM `Users`;";
     echo $mySQL->Query($sql, true);
 } else if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getpost'])) {
     $post = $_GET['getpost'];
     $sql = "SELECT * FROM `Entries` WHERE post_id = $post;";
     echo $mySQL->Query($sql, true);
-} else if ($_SERVER['REQUEST_METHOD'] === 'PUT' && isset($_GET['id'])) {
-    $postId = $_GET['id'];
+}
+//GetPosts is a 'view' in the database, 
+else if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getallposts'])) {
+    $sql = "SELECT * FROM `GetPosts`;";
+    echo $mySQL->Query($sql, true);
+}
+
+//API endpoints for the PUT method. Used to send data to server and subsequently to database.
+//"CALL UpdatePost()" and "CALL UpdateUser()" refers to stored procedures in the database.
+else if ($_SERVER['REQUEST_METHOD'] === 'PUT' && isset($_GET['postid'])) {
+    $id = $_GET['postid'];
     $post = json_decode(file_get_contents('php://input'));
-    $sql = "CALL UpdatePost('$post->post_title','$post->post_description','$post->post_category','$post->expiration_date','$postId');";
+    $sql = "CALL UpdatePost('$post->post_title','$post->post_description','$post->post_category','$post->expiration_date','$id');";
+    echo $mySQL->Query($sql, false);
+} else if ($_SERVER['REQUEST_METHOD'] === 'PUT' && isset($_GET['userid'])) {
+    $id = $_GET['userid'];
+    $user = json_decode(file_get_contents('php://input'));
+    $sql = "CALL UpdateUser('$user->first_name','$user->last_name','$user->email','$user->is_business','$user->pass','$id');";
     echo $mySQL->Query($sql, false);
 }
