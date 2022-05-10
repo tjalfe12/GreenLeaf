d225cd46e941b43fc17c5426cb0cd2cddbf25017
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2022-05-10 16:11:26 +0200

    Added more api calls

--- greenleaf/src/App.js
+++ greenleaf/src/App.js
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     async function getPosts() {
       const url =
-        "http://localhost:3000/greenleaf/src/Backend/api.php?table=Tags";
+        "http://localhost:3000/greenleaf/src/Backend/api.php?getallposts";
       const response = await fetch(url);
       const data = await response.json();
       console.log(data["data"]);
--- greenleaf/src/Backend/api.php
+++ greenleaf/src/Backend/api.php
@@ -9,8 +9,19 @@ $request_method = $_SERVER['REQUEST_METHOD'];
 $mySQL = new MySQL(true);
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['table'])) {
-    $postId = $_GET['table'];
-    $sql = "SELECT * FROM $postId;";
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getallposts'])) {
+    $sql = "SELECT * FROM `GetPosts`;";
     echo $mySQL->Query($sql, true);
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getusers'])) {
+    $sql = "SELECT * FROM `GetPosts`;";
+    echo $mySQL->Query($sql, true);
+} else if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['getpost'])) {
+    $post = $_GET['getpost'];
+    $sql = "SELECT * FROM `Entries` WHERE post_id = $post;";
+    echo $mySQL->Query($sql, true);
+} else if ($_SERVER['REQUEST_METHOD'] === 'PUT' && isset($_GET['id'])) {
+    $postId = $_GET['id'];
+    $post = json_decode(file_get_contents('php://input'));
+    $sql = "CALL UpdatePost('$post->post_title','$post->post_description','$post->post_category','$post->expiration_date','$postId');";
+    echo $mySQL->Query($sql, false);
 }
