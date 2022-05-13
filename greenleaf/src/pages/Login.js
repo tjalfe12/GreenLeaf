dd296671b84532e33e098f2ec4179a1148bb1386
Daniel692 <birkusdaniel6@gmail.com>
2022-05-13 09:56:02 +0200

    added basic html elements

--- greenleaf/src/pages/Login.js
+++ greenleaf/src/pages/Login.js
@@ -1,3 +1,9 @@
 export default function Login() {
-  return <h1>Login</h1>;
+  return <div>
+    <h1>Login</h1>;
+   <input type="text" placeholder="Name"/>
+   <input type="password" placeholder="Password"/>
+   <br/>
+   <input type="submit" value="Login"></input>
+  </div>
 }
