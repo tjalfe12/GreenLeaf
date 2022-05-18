723a6f52790ae6558d81be7c6e9b59d92826da6b
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2022-05-18 12:02:33 +0200

    added imgs

--- greenleaf/src/components/buttons/DeleteButton.js
+++ greenleaf/src/components/buttons/DeleteButton.js
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function DeleteButton({ post_id }) {
+  const navigate = useNavigate();
+
   async function deletePost() {
     const url = `http://www.sabox.dk/backend/api.php?deletepost=${post_id}`;
     const response = await fetch(url);
     const data = await response.text();
     console.log("POST WAS DELETED");
     console.log(data);
+    navigate(`/posts`);
   }
 
   return (
