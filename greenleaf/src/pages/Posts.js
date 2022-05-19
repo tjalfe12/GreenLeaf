c1507db76c622d231e9f72695a602c9121cf4cf8
tjalfe12 <61150285+tjalfe12@users.noreply.github.com>
2022-05-19 12:43:51 +0200

    Update Posts.js

--- greenleaf/src/pages/Posts.js
+++ greenleaf/src/pages/Posts.js
@@ -1,28 +1,27 @@
 import { useState, useEffect } from "react";
-import SinglePost from "../components/Post"
+import SinglePost from "../components/Post";
 
 export default function Posts() {
-const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        async function getPosts() {
-            const url = "http://www.sabox.dk/backend/api.php?getallposts";
-            const response = await fetch(url);
-            const data = await response.json();
-            console.log(data.data)
-            setPosts(data.data);
-        }
-        getPosts();
-    }, []);
+  useEffect(() => {
+    async function getPosts() {
+      const url = "http://www.sabox.dk/backend/api.php?getallposts";
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.data);
+      setPosts(data.data);
+    }
+    getPosts();
+  }, []);
 
-    return (
-        <section className="page">
-            <section className="grid-container">
-                {posts.map(post => (
-                    <SinglePost post={post} key={post.post_id}/>
-                ))}
-
-            </section>
-        </section>
-    );
+  return (
+    <section className="page">
+      <section className="grid-container">
+        {posts.map((post) => (
+          <SinglePost post={post} key={post.post_id} />
+        ))}
+      </section>
+    </section>
+  );
 }
